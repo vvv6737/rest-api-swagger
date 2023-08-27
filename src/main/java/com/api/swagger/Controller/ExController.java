3a852771c6d524d8aed55285b74ec44506fe5810
jeonghoon <vvv6737@naver.com>
2023-08-27 15:23:22 +0900

    스웨거 commit

---
+++ src/main/java/com/api/swagger/Controller/ExController.java
@@ -0,0 +1,26 @@
+package com.api.swagger.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+
+@RestController
+@RequestMapping("/ex")
+public class ExController {
+
+    @GetMapping(value = "/ex", name = "예시")
+    public HashMap<String, Object> ex() {
+        HashMap<String, Object> resMap = new HashMap<>();
+        resMap.put("의히","으으");
+        resMap.put("의히1","으으");
+        resMap.put("의히2","으으");
+        resMap.put("의히3","으으");
+        resMap.put("의히4","으으");
+        System.out.println(resMap);
+        return resMap;
+    }
+
+}
